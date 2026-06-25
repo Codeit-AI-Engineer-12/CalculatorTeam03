67f0d1c365ebc1ec230348c1e61c75383eae6be2
joelchoi85 <jymania@naver.com>
2026-06-25 14:22:33 +0900

    Fix: multiply 자연수만 계산할 수 있게 수정

--- ops/multiply.py
+++ ops/multiply.py
@@ -1,4 +1,4 @@
-def multiply(a: int, b: int) -> int:
+def multiply(a: int, b: int) -> int | None:
     """int형 두 수를 곱합니다
 
     Args:
@@ -7,8 +7,12 @@ def multiply(a: int, b: int) -> int:
 
     Returns:
         int: a와 b를 곱한 값
+        None: a나 b가 정상적인 값이 아닐 경우
     """
     if not isinstance(a, int) or not isinstance(b, int):
         print("입력되는 두 수 모두 int형이어야 합니다")
+        return None
+    if a < 0 or b < 0:
+        print("자연수만 다룹니다. 흐즈믈르그")
         return None
     return a * b
